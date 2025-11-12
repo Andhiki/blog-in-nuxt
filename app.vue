a205741a7b4764f33b6f52734add519558944e79
Andre <andhiki28@gmail.com>
2025-11-12 12:55:16 +0700

    feat: init

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-red-200">
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <Button> Click me! </Button>
+    <div class="bg-blue-200"><!-- Content goes here -->dsadsa</div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/fonts", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+    alias: {
+      "@": fileURLToPath(new URL("./", import.meta.url)),
+    },
+  },
+});
